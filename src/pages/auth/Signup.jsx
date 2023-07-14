5031233fd122e64d7cb46c250ff0726a14092664
Raushan <raushankp75@gmail.com>
2023-07-14 11:29:25 +0530

    Added functionality to show and hide password in Login and Signup page

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Button, CardMedia, TextField, Typography } from '@mui/material'
+import { BiSolidHide, BiSolidShow } from 'react-icons/bi'
 
 const Signup = () => {
 
     const profileUpload = 'https://static.thenounproject.com/png/5657068-200.png'
+
+    const [show, setShow] = useState(false)
+
+    // hide and show password
+    const handleShowHide = () => {
+        setShow(!show)
+    }
     return (
         <Box>
             {/* <Typography sx={{ fontSize: '18px', color:'gray', textAlign:'center' }}>Sign up to chat with your friends</Typography> */}
 
             <form>
-            <Box sx={{ display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
                     <Button
                         // variant="contained"
                         component="label"
@@ -29,10 +37,10 @@ const Signup = () => {
                             type="file"
                             hidden
                             accept='image/*'
-                            // onChange={(event) => {
-                            //     loadfile(event);
-                            //     postDetails(event.target.files[0])
-                            // }}
+                        // onChange={(event) => {
+                        //     loadfile(event);
+                        //     postDetails(event.target.files[0])
+                        // }}
                         />
                     </Button>
 
@@ -43,10 +51,20 @@ const Signup = () => {
 
                 <TextField variant='standard' type='name' name='name' label='name' placeholder='Enter name' fullWidth required sx={{ margin: '4px 0', borderBottom: '1px solid gray' }} />
                 <TextField variant='standard' type='email' name='email' label='Email' placeholder='Enter email' fullWidth required sx={{ margin: '4px 0', borderBottom: '1px solid gray' }} />
-                <TextField variant='standard' type='password' name='password' label='Password' placeholder='Enter password' fullWidth required sx={{ margin: '4px 0', borderBottom: '1px solid gray' }} />
-                <TextField variant='standard' type='password' name='conformPassword' label='conformPassword' placeholder='Enter conform password' fullWidth required sx={{ margin: '4px 0', borderBottom: '1px solid gray' }} />
+                <Box sx={{ position: 'relative' }}>
+                    <TextField variant='standard' type={show ? 'text' : 'password'} name='password' label='Password' placeholder='Enter password' fullWidth required sx={{ margin: '4px 0', borderBottom: '1px solid gray' }} />
+                    <Button onClick={handleShowHide} sx={{ position: 'absolute', top: '15px', right: '0' }}>
+                        {show ? <BiSolidHide size={25} /> : <BiSolidShow size={25} />}
+                    </Button>
+                </Box>
+                <Box sx={{ position: 'relative' }}>
+                    <TextField variant='standard' type={show ? 'text' : 'password'} name='conformPassword' label='conformPassword' placeholder='Enter conform password' fullWidth required sx={{ margin: '4px 0', borderBottom: '1px solid gray' }} />
+                    <Button onClick={handleShowHide} sx={{ position: 'absolute', top: '15px', right: '0' }}>
+                        {show ? <BiSolidHide size={25} /> : <BiSolidShow size={25} />}
+                    </Button>
+                </Box>
 
-                <Button type='submit' color='primary' variant="contained" sx={{ margin: '10px 0', fontWeight:600 }} fullWidth>Signup</Button>
+                <Button type='submit' color='primary' variant="contained" sx={{ margin: '10px 0', fontWeight: 600 }} fullWidth>Signup</Button>
             </form>
         </Box>
     )
