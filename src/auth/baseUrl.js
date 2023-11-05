5ae80050340772375e102ccb4e4ab543f4b1ab15
Raushan <raushankp75@gmail.com>
2023-11-05 15:12:16 +0530

    change baseUrl to deployed Backend URL

--- src/auth/baseUrl.js
+++ src/auth/baseUrl.js
@@ -1,3 +1,3 @@
 // export const baseUrl ='http://localhost:8000'
-export const baseUrl ='https://chat-app-gcpa.onrender.com'
-// export const baseUrl =['https://chat-app-gcpa.onrender.com' || 'http://localhost:8000']
+// export const baseUrl ='https://chat-app-gcpa.onrender.com'
+export const baseUrl =['https://chat-app-gcpa.onrender.com' || 'http://localhost:8000']
--- src/services/helper.js
+++ src/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL='http://localhost:8000';
+export const BASE_URL=['https://chat-app-gcpa.onrender.com' || 'http://localhost:8000'];
 
 export const myAxios=axios.create({
     baseURL:BASE_URL
