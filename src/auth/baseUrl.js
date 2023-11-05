8a5e2ccc513e659dbe26999d280486122fda609e
Raushan <raushankp75@gmail.com>
2023-11-05 14:59:40 +0530

    change baseUrl to deployed Backend URL

--- src/auth/baseUrl.js
+++ src/auth/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl ='http://localhost:8000'
+// export const baseUrl ='http://localhost:8000'
+export const baseUrl =['https://chat-app-gcpa.onrender.com' || 'http://localhost:8000']
